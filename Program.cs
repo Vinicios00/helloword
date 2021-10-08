10262a67c9533088a346c3de47441de57b430ead
Vinicios Gimenez de Souza <gimenez.vinicios05@gmail.com>
2021-10-08 14:28:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;                   // MODULOS IMPORTADOS
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program // INICIO DO PROGAMA
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World\n");
+
+
+            Console.Write("Meu nome é Vini\n");
+
+            Console.ReadLine(); // O PROGAMA ESPERA DO USUARIO O ENTER PARA FECHAR
+
+            // PROGAMA A SER EXECUTAVEL
+
+
+        }
+    } // FIM DO PROGAMA
+}
+
+// Executar o progama :  dotnet run
+
+
